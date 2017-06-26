195fc3fd9cf1eff762b6003ce0a0b3c2e94a778b
wuxiaokui <xk.wu@hnair.com>
2017-06-26 17:58:51 +0800

    1

---
+++ 启动广告页/advertiseDemo/AdvertiseViewController.h
@@ -0,0 +1,15 @@
+//
+//  AdvertiseViewController.h
+//  zhibo
+//
+//  Created by 周焕强 on 16/5/17.
+//  Copyright © 2016年 zhq. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+//点击进入此页面
+@interface AdvertiseViewController : UIViewController
+
+@property (nonatomic, copy) NSString *adUrl;
+
+@end
